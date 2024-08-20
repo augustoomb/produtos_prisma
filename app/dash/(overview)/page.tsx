25776ab8f83e54c51db34cc9df8e0d2a8ab55b43
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-08-20 15:40:59 -0300

    feat: home page dash

---
+++ app/dash/(overview)/page.tsx
@@ -0,0 +1,24 @@
+
+import { Button } from "@/components/ui/button"
+// import { DownloadIcon } from "@/components/clients/icons"
+
+
+export default function Dash() {
+  return (
+    <main className="flex-grow p-6">
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-lg font-medium">Dash</h1>
+          {/* <Button
+            type="button"
+            className="px-2 py-1 bg-gray-800 text-white rounded-lg flex items-center space-x-2 text-sm"
+          >
+            <DownloadIcon className="w-4 h-4" />
+            <span>+ Cliente</span>
+          </Button> */}
+        </div>
+        {/* <ClientsTable /> */}
+      </main>
+  )
+}
+
+
