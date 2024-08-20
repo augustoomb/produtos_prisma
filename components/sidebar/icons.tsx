aba4e53c23c1d4b14cbde194224d38d67cab006f
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-08-20 15:42:36 -0300

    feat: sidebar

---
+++ components/sidebar/icons.tsx
@@ -0,0 +1,168 @@
+
+import { IconProps } from '@/types/IconProps'
+
+export function HomeIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+      <polyline points="9 22 9 12 15 12 15 22" />
+    </svg>
+  )
+}
+
+export function WalletIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1" />
+      <path d="M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4" />
+    </svg>
+  )
+}
+
+export function UsersIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+      <circle cx="9" cy="7" r="4" />
+      <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+      <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+    </svg>
+  )
+}
+
+export function ShoppingCartIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="9" cy="21" r="1" />
+      <circle cx="20" cy="21" r="1" />
+      <path d="M1 1h4l2 12h13l3-7H6" />
+    </svg>
+  )
+}
+
+export function ServicesIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M22 7h-4l-3 9H5l1-3h4l1-3H5L4 7H2" />
+      <path d="M16 5a2 2 0 1 0-4 0v4a2 2 0 1 0 4 0" />
+    </svg>
+  )
+}
+
+export function SettingsIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="3" />
+      <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09a1.65 1.65 0 0 0-1-1.51 1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.09a1.65 1.65 0 0 0 1.51-1 1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06a1.65 1.65 0 0 0 1.82.33h.09a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82v.09a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z" />
+    </svg>
+  )
+}
+
+export function ProductsIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M21 16V8a2 2 0 0 0-1-1.73L12 2 4 6.27A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73L12 22l8-4.27A2 2 0 0 0 21 16z" />
+      <polyline points="3.29 7 12 12 20.71 7" />
+      <line x1="12" y1="22" x2="12" y2="12" />
+    </svg>
+  )
+}
+
+// export function TicketIcon(props: IconProps) {
+//   return (
+//     <svg
+//       {...props}
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     >
+//       <path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" />
+//       <path d="M13 5v2" />
+//       <path d="M13 17v2" />
+//       <path d="M13 11v2" />
+//     </svg>
+//   )
+// }
---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import { HomeIcon, WalletIcon, UsersIcon, ShoppingCartIcon, ServicesIcon, 
+    SettingsIcon, ProductsIcon } from './icons'
+import LogoMenu from '@/public/logo-F3F4F6.png'
+import { MenuItem } from '@/types/MenuItem'
+
+const links: MenuItem[] = [
+    { name: 'Home', href: '/dash', icon: HomeIcon },
+    { name: 'Clientes', href: '/dash/clients', icon: UsersIcon },
+    // { name: 'Compras', href: '/dash', icon: ShoppingCartIcon },
+    // { name: 'Produtos', href: '/dash', icon: ProductsIcon },
+    // { name: 'Configurações', href: '/dash', icon: SettingsIcon },
+    // { name: 'Serviços', href: '/dash', icon: ServicesIcon },
+    // { name: 'Vendas', href: '/dash', icon: WalletIcon },
+]
+
+export default function Sidebar() {
+    const pathname = usePathname();
+
+    return(
+        <aside className="sticky top-0 h-screen w-56 bg-gray-100 text-gray-800 p-4">
+            <div className="flex items-center mb-4 space-x-1">
+                <Image
+                    src={ LogoMenu }
+                    width="30"
+                    height="30"
+                    alt="Company Logo"
+                    style={{ aspectRatio: "30/30", objectFit: "cover" }}
+                />
+                <h1 className="text-lg font-medium">Pagfreela</h1>
+            </div>
+            <nav className="space-y-2">
+
+                { links.map((link) => {
+                    const LinkIcon = link.icon;
+                    return(
+                        <Link 
+                            key={ link.name } 
+                            href={ link.href } 
+                            className={ clsx(
+                                "w-full flex items-center space-x-2 hover:bg-gray-200 active:bg-gray-300 py-2 px-2 rounded-lg text-gray-500",
+                                {
+                                    "bg-gray-200 text-gray-800": pathname === link.href
+                                }
+                            ) }>
+                                <LinkIcon className="w-4 h-4" />
+                                <span className="text-sm font-medium">{ link.name }</span>
+                            </Link>
+                    )
+                }) }
+            </nav>
+        </aside>
+    )
+}
