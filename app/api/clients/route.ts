3e89f1dbcb6e4086fd59904ca4ac3994353c2c6d
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-08-27 10:56:15 -0300

    feat: rota back-end para listar clientes e salvar clientes

---
+++ app/api/clients/route.ts
@@ -0,0 +1,53 @@
+import prisma from '@/lib/prisma';
+import { Client } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
+import { clientSchema } from '@/types/zod';
+
+// GET CLIENTS
+export async function GET(req: Request, res: NextApiResponse) {
+   try {
+        const clients: Client[] = await prisma.client.findMany({
+            // where: { published: true },
+            // include: {
+            //   author: {
+            //     select: { name: true },
+            //   },
+            // },
+        });
+
+        return NextResponse.json(clients);
+   } catch (error) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+   }
+}
+
+// SAVE CLIENTS
+export async function POST(req: Request, res: NextApiResponse) {
+
+    try {
+        const dataClient = await req.json();
+
+        const validatedClient = clientSchema.safeParse(dataClient)
+
+        if (!validatedClient.success) {
+            return NextResponse.json({
+                errors: validatedClient.error.flatten().fieldErrors
+            }, { status: 400 });
+        }
+
+        const { name, email, phone } = dataClient
+
+        await prisma.client.create({
+            data: {
+                name,
+                email,
+                phone,
+            },
+        });
+
+        return NextResponse.json({ message: 'Cliente criado com sucesso' });
+    } catch (error) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
