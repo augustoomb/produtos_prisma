0093a50ff1aaa4dead84fe4313e58b5f1bffa691
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-08-27 10:55:49 -0300

    feat: client actions para receber solicitação de interação com a entidade Client do Prisma

--- actions/client.ts
+++ actions/client.ts
@@ -1,15 +1,72 @@
-import prisma from '@/lib/prisma';
 import { Client } from "@prisma/client";
+import { clientSchema } from '@/types/zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function getClients(): Promise<Client[]> {
-    const clients: Client[] = await prisma.client.findMany({
-        // where: { published: true },
-        // include: {
-        //   author: {
-        //     select: { name: true },
-        //   },
-        // },
-    });
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/clients`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-    return clients;
+        const clients = await response.json();
+
+        return clients
+    } catch (error) {
+        return []
+    }
+}
+
+export async function createClient(prevState: any, formData: FormData) {
+    try {
+        const validatedClient = clientSchema.safeParse({
+            name: formData.get('name'),
+            email: formData.get('email'),
+            phone: formData.get('phone'),
+        })
+
+        if (!validatedClient.success) {
+            return {
+                status: "error",
+                errors: validatedClient.error.flatten().fieldErrors
+            }
+        }
+
+        const { name, email, phone } = validatedClient.data     
+
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/clients`, {
+            method: 'POST',
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                phone: phone,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            return {
+                status: "error",
+                errors: "Erro desconhecido",
+            }
+        }
+
+        await response.json();
+
+        return {
+            status: "success",
+            errors: {},
+        }        
+        
+    } catch (error) {
+        return {
+            status: "error",
+            errors: "Erro desconhecido",
+        }
+    }
 }
