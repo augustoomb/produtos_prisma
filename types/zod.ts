c0b559347ced856cc0adccde57a69bb7704716e9
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-09-09 17:04:09 -0300

    feat: validação zod para Product

--- types/zod.ts
+++ types/zod.ts
@@ -16,3 +16,20 @@ export const clientSchema = z.object({
         message: "Telefone deve ter 11 caracteres",
     })
 });
+
+export const productSchema = z.object({
+    id: z.number().int({
+        message: 'O id deve ser um número inteiro',
+    }).optional(),
+    name: z.string().min(5, {
+        message: "Nome deve ter mais de 1 caracter",
+    }).max(50, {
+        message: "Nome deve ter menos de 50 caracteres",
+    }),
+    price: z.number().positive({
+        message: 'O preço deve ser um valor maior que zero',
+    }),
+    description: z.string().min(5, {
+        message: "Descrição deve ter mais de 5 caracteres",
+    }).optional()
+});
