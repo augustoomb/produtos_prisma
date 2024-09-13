f41beef05d9275c347ce537c13324166f45478a6
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-09-13 18:41:50 -0300

    fix: corrigindo uso do componente de adição de clientes a partir da pagina principal de clientes

--- app/dash/clients/page.tsx
+++ app/dash/clients/page.tsx
@@ -1,31 +1,16 @@
 import { columns } from "@/components/clients/columns-table"
 import { DataTable } from "@/components/clients/clients-table"
 import { getClients } from "@/actions/client";
-import { Client } from "@prisma/client";
-import { createClient } from "@/actions/client";
-import AddEntity from "@/components/reutilizaveis/add-entity";
 import { Suspense } from 'react'
+import AddClient from "@/components/clients/add-client";
 
 export default async function Clients() {
-
-  const clientFields = [
-    { id: "name", name: "name", label: "Nome", type: "text", placeholder: "Fulano de Tal" },
-    { id: "email", name: "email", label: "E-mail", type: "email", placeholder: "fulano@mail.com" },
-    { id: "phone", name: "phone", label: "Telefone", type: "text", placeholder: "XX999998888" },
-  ];
-
-
   return (
     <main className="flex-grow p-2 md:p-6">
       <div className="flex justify-between items-center">
         <h1 className="text-lg font-medium">Clientes</h1>
         <div className="hidden md:block">
-        <AddEntity
-          title="Cliente"
-          description="Adicionar um novo cliente"
-          fields={clientFields}
-          createEntity={createClient}
-        />
+          <AddClient />
         </div>
       </div>      
       <Suspense fallback={<p>Carregando...</p>}> 
