12f8710fdb60764a3227287bf6ec81205471dd3d
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-09-18 16:18:08 -0300

    refactor: removendo código não utilizando da página principal de produto

--- app/dash/products/page.tsx
+++ app/dash/products/page.tsx
@@ -1,29 +1,16 @@
 import { columns } from "@/components/products/columns-table"
 import { DataTable } from "@/components/products/products-table"
 import { getProducts } from "@/actions/product";
-import { Product } from "@prisma/client";
-import AddEntity from "@/components/reutilizaveis/add-entity";
-import { createProduct } from "@/actions/product";
 import { Suspense } from 'react'
+import AddProduct from "@/components/products/add-product";
 
 export default async function Products() {
-  const productFields = [
-    { id: "name", name: "name", label: "Nome", type: "text", placeholder: "Fone de ouvido" },
-    { id: "price", name: "price", label: "Preço", type: "number", placeholder: "0,00" },
-    { id: "description", name: "description", label: "Descrição", type: "text", placeholder: "Produto feito com ..." },
-  ];
-
-  return (
-    <main className="flex-grow p-2 md:p-6">
+    return (
+      <main className="flex-grow p-2 md:p-6">
       <div className="flex justify-between items-center">
         <h1 className="text-lg font-medium">Produtos</h1>
         <div className="hidden md:block">
-        <AddEntity
-          title="Produto"
-          description="Adicionar um novo produto"
-          fields={productFields}
-          createEntity={createProduct}
-        />
+          <AddProduct />
         </div>
       </div>      
       <Suspense fallback={<p>Carregando...</p>}> 
