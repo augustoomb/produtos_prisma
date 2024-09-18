4d9bba8410c50d1e5c780c8d633181b45e00aeb6
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-09-18 11:08:55 -0300

    refactor: função fetch é agora reutilizavel

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,33 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { revalidatePath } from 'next/cache';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function fetcher(method: string, body: any | null, model: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/${model}`, {
+      method: method,
+      body: JSON.stringify(body),
+      headers: {
+          'Content-Type': 'application/json',
+      },
+  });
+
+  if (!response.ok) {
+      return {
+          status: "error",
+          errors: { erro: "Erro ao criar. Verifique a disponibilidade do seu banco de dados." },
+      }
+  }
+
+  await response.json();
+
+  revalidatePath(`/dash/${model}`);
+
+  return {
+      status: "success",
+      errors: {},
+  }       
 }
