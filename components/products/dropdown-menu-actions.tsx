2bc7dac62ee00272f31ec41d716ccd10e22b136a
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-09-18 16:21:14 -0300

    refactor: ações do dropdown menu de edição de produtos agora usam funções genéricas, economizando código

--- components/products/dropdown-menu-actions.tsx
+++ components/products/dropdown-menu-actions.tsx
@@ -1,9 +1,7 @@
 import {
     Dialog,
-    DialogClose,
     DialogContent,
     DialogDescription,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
@@ -17,8 +15,6 @@ import {
     DropdownMenuLabel,
 } from "@/components/ui/dropdown-menu"
 
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { MoreHorizontal } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
@@ -26,6 +22,8 @@ import { Product } from "@prisma/client";
 import { useFormState} from "react-dom"
 import { deleteProduct, updateProduct } from "@/actions/product"
 import { toast } from "sonner" 
+import FormProduct from "./form-product"
+import DialogDeleteConfirmation from "@/components/general/dialolog-delete-confirmation"
 
 export default function DropdownMenuActions({ product }: {product: Product}) {
 
@@ -36,8 +34,7 @@ export default function DropdownMenuActions({ product }: {product: Product}) {
     }  
     const [dialog, setDialog] = useState('')
 
-
-    // DELETE CLIENT
+    // DELETE PRODUCT
     const formInitialState = {
         status: "",
         errors: {},
@@ -45,41 +42,30 @@ export default function DropdownMenuActions({ product }: {product: Product}) {
 
     const [dialogIsOpen, setDialogIsOpen] = useState(false);
     const [formState, formAction] = useFormState(async (prevState: any, formData: FormData) => {
-
-        // let result = {};
+        
         const submitButton = formData.get("submitButton");
+        const textMessage = submitButton === "deleteButton" ? "excluído" : "editado";
 
-        if (submitButton === "deleteButton") {
-            const result = await deleteProduct(prevState, formData);
+        let result;
 
-            if (result.status === "success") {
-                setDialogIsOpen(false);
-    
-                toast.success("Produto excluído", {
-                    description: "Produto foi excluído com sucesso",
-                })
-            }    
-    
-            return result;
-
+        if(submitButton === "deleteButton") {
+            result = await deleteProduct(prevState, formData);
         } else {
-            const result = await updateProduct(prevState, formData);
-
-            if (result.status === "success") {
-                setDialogIsOpen(false);
-    
-                toast.success("Produto editado", {
-                    description: "Produto foi editado com sucesso",
-                })
-            }    
-    
-            return result;
+            result = await updateProduct(prevState, formData);
         }
+
+        if (result.status === "success") {
+            setDialogIsOpen(false);
+
+            toast.success(`Produto ${ textMessage }`, {
+                description: `Produto foi ${ textMessage } com sucesso`,
+            })
+        } 
+
+        return result;
         
     }, formInitialState);
 
-
-    // COMPONENT
     return(
         <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
             <DropdownMenu>
@@ -110,111 +96,24 @@ export default function DropdownMenuActions({ product }: {product: Product}) {
                 </DropdownMenuContent>
             </DropdownMenu>
             <DialogContent>
-            {dialog === Dialogs.dialog1
-                ? (
-                    <form action={formAction}>
-                        <DialogHeader className="mt-2 mb-4">
-                            <DialogTitle className="text-red-500">Apagar produto?</DialogTitle>
-                            <DialogDescription>
-                                { `Tem certeza que deseja apagar o produto ${product.name}? Essa ação não pode ser desfeita.` }
-                            </DialogDescription>
-                        </DialogHeader>
+                {dialog === Dialogs.dialog1
+                    ? (
+                        <DialogDeleteConfirmation formAction={ formAction } id={product.id} />
+                    )
+                    : (                   
+                        <>
+                            <DialogHeader className="mt-2 mb-4">
+                                <DialogTitle className="">Editar Produto</DialogTitle>
+                                <DialogDescription>
+                                    Edição de produto cadastrado
+                                </DialogDescription>
+                            </DialogHeader>
 
-                        <input value={product.id} name="id" readOnly hidden />
-                            
-                        <DialogFooter>
-                            <DialogClose asChild>
-                            <Button type="button" variant="outline">
-                                Cancelar
-                            </Button>
-                            </DialogClose>
-                            <Button variant={"destructive"} type="submit" name="submitButton" value="deleteButton">Confirmar</Button>
-                        </DialogFooter>
-                    </form>
-                )
-                : (
-                    <form action={formAction}>
-                        <DialogHeader className="mt-2 mb-4">
-                            <DialogTitle className="">Editar produto</DialogTitle>
-                            <DialogDescription>
-                                Edição de produto cadastrado
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="idProduct" className="text-right">
-                                    ID
-                                </Label>
-                                <Input
-                                    id="idProduct"
-                                    name="idProduct"
-                                    placeholder="XXX"
-                                    className="col-span-3 bg-slate-50 pointer-events-none select-none"
-                                    defaultValue={product.id}
-                                    readOnly
-                                    // disabled
-                                />                            
-                            </div>
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="name" className="text-right">
-                                    Nome
-                                </Label>
-                                <Input
-                                    id="name"
-                                    name="name"
-                                    placeholder="Fone de ouvido"
-                                    className="col-span-3"
-                                    defaultValue={product.name}
-                                />                            
-                            </div>
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="price" className="text-right">
-                                    Preço
-                                </Label>
-                                <Input
-                                    id="price"
-                                    name="price"
-                                    type="number"
-                                    placeholder={ `RS 0,00` }
-                                    className="col-span-3"
-                                    defaultValue={Number(product.price)}
-                                />
-                            </div>
-                            <div className="grid grid-cols-4 items-center gap-4">
-                                <Label htmlFor="description" className="text-right">
-                                    Descrição
-                                </Label>
-                                <Input
-                                    id="description"
-                                    name="description"
-                                    type="text"
-                                    placeholder="Ex: Fone de ouvido com conexão bluetooth"
-                                    className="col-span-3"
-                                    defaultValue={product.description || ''}
-                                />
-                            </div>
-                        </div>
-                        <DialogFooter className="flex flex-col">
-                            <DialogClose asChild>
-                                <Button type="button" variant="outline">
-                                    Cancelar
-                                </Button>
-                            </DialogClose>
-                            <Button type="submit" name="submitButton" value="editButton">Salvar</Button>                                             
-                        </DialogFooter>        
-                        <div className="text-sm text-red-500 text-center">
-                                {formState.errors && (
-                                    <>
-                                        {Object.entries(formState.errors).map(([key, error]) => (
-                                            error && <p key={key}>{String(error)}</p>
-                                        ))}                                
-                                    </>
-                                )}
-                        </div>
-                    </form>
-                )
-            }
-            </DialogContent>
+                            <FormProduct formAction={formAction} formState={formState} product={product}/>
+                        </>
+                    )
+                }
+            </DialogContent>            
         </Dialog>
     )
 }
